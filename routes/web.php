08ca729b42bb1d1d26a7eb67138deb40bf98b62b
DarkPain <darkpain87@gmail.com>
2017-06-10 01:27:19 +0300

    trying botman

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,13 @@ Route::get('/', function () {
 
 Route::get('webhook', function () {
     $botman = app('botman');
-    $botman->verifyServices('Bemqsiemens123123123123!');
+    return $botman->verifyServices('Bemqsiemens123123123123!');
 
-// give the bot something to listen for.
-    $botman->hears('hello', function (BotMan $bot) {
-        $bot->reply('Hello yourself.');
-    });
-
-// start listening
-    $botman->listen();
+//// give the bot something to listen for.
+//    $botman->hears('hello', function (BotMan $bot) {
+//        $bot->reply('Hello yourself.');
+//    });
+//
+//// start listening
+//    $botman->listen();
 });
