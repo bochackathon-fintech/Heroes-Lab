f79f8b45d3462314f2f89076f57f8ae28c6aa7dd
Proddie <p.alampritis@gmail.com>
2017-06-10 12:57:17 +0100

    welcome.blade update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,42 @@
             font-size: 84px;
         }
 
+        .welcome_paragraph {
+            font-size: : 50px;
+        }
+
+
+
+        @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+        @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+        @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+
+        .fade-in {
+            opacity:0;  /* make things invisible upon start */
+            -webkit-animation:fadeIn ease-in 1;  /* call our keyframe named fadeIn, use animattion ease-in and repeat it only 1 time */
+            -moz-animation:fadeIn ease-in 1;
+            animation:fadeIn ease-in 1;
+
+            -webkit-animation-fill-mode:forwards;  /* this makes sure that after animation is done we remain at the last keyframe value (opacity: 1)*/
+            -moz-animation-fill-mode:forwards;
+            animation-fill-mode:forwards;
+
+            -webkit-animation-duration:3s;
+            -moz-animation-duration:3s;
+            animation-duration:3s;
+        }
+
+        .fade-in.title {
+            -webkit-animation-delay: 1s;
+            -moz-animation-delay: 1s;
+            animation-delay: 1s;
+        }
+
+        .fade-in.welcome_paragraph {
+            -webkit-animation-delay: 4s;
+            -moz-animation-delay:4s;
+            animation-delay: 4s;
+        }
         .links > a {
             color: #636b6f;
             padding: 0 25px;
@@ -65,6 +101,26 @@
     </style>
 </head>
 <body>
+<script>
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: "134601750423617",
+            xfbml: true,
+            version: "v2.6"
+        });
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+</script>
 <div class="flex-center position-ref full-height">
     @if (Route::has('login'))
         <div class="top-right links">
@@ -77,11 +133,27 @@
         </div>
     @endif
 
-    <div class="content">
-        <div class="title m-b-md">
-            Welcome to MetisBot homepage
+
+        <div class= "fade-in">
+
+        <div class="content">
+            <div class="title">
+                Welcome to MetisBot homepage
+            </div>
+            <div class="welcome_paragraph">
+                <p>
+                    Hi! My name is Metis, your financial assistance. I will walk you through what i can do below :)
+                </p>
+            </div>
+            <div class="fb-messengermessageus"
+                 messenger_app_id="134601750423617"
+                 page_id="372522269811171"
+                 color="blue"
+                 size="standard" >
+            </div>
+
         </div>
     </div>
-</div>
+
 </body>
 </html>
