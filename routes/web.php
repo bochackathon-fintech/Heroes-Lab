b6c79d80f3647d46f5b86a3c6f4dad2f7f4b1c6c
DarkPain <darkpain87@gmail.com>
2017-06-11 00:22:47 +0300

    fixes in conversation

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::any('getCookie', 'BotManController@getCookie');
 
 Route::any('createuser','BotManController@createUser');
 
+Route::get('test', function () {
+    $api = new \App\Helpers\APIHelper(env('BOC_AUTH_PROVIDER_NAME'), env('BOC_AUTH_ID'), env('BOC_TOKEN'));
+    return $api->getBanks();
+});
