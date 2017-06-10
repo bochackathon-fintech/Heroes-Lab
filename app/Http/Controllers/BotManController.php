83d6dca1dd344d198284aa7798f785093d896509
DarkPain <darkpain87@gmail.com>
2017-06-10 03:09:00 +0300

    bot demo

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -19,7 +19,7 @@ class BotManController extends Controller
             $bot->reply('Hi there :)');
         });
 
-        $botman->hears('Start conversation', function (BotMan $bot) {
+        $botman->hears('start', function (BotMan $bot) {
             $bot->startConversation(new ExampleConversation());
         });
 
