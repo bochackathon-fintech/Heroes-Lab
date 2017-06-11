10bdab13f7687dfbf035079ccb1a87f1305b8937
DarkPain <darkpain87@gmail.com>
2017-06-11 12:41:14 +0300

    fix menu

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
             ->create()
             ->each(function (User $user) {
                 // $user->bankAccounts()->save(factory(App\UserBankAccount::class)->create(['user_id' => $user->id]));
-                $user->transactions()->save(factory(App\Transaction::class, 1000)->create(['user_id' => $user->id]));
+                $transactions = factory(App\Transaction::class, 1000)->create(['user_id' => $user->id]);
+                $user->transactions()->saveMany($transactions);
             });
     }
 }
