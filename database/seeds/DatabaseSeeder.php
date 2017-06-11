2f6dca72d73e67992209869c72a142c993cd7cec
DarkPain <darkpain87@gmail.com>
2017-06-11 12:15:39 +0300

    seed tests

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,15 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Transaction::class, 1000)->create();
         //$this->call(UsersTableSeeder::class);
         // $this->call(TransactionsTableSeeder::class);
         //$this->call(AccountsTableSeeder::class);
-//        $users = factory(App\User::class, 100)
-//            ->create()
-//            ->each(function (User $user) {
-//                $user->bankAccounts()->save(factory(App\UserBankAccount::class)->create(['user_id' => $user->id]));
-//                $user->transactions()->save(factory(App\Transaction::class)->create(['user_id' => $user->id]));
-//            });
+        $users = factory(User::class, 100)
+            ->create()
+            ->each(function (User $user) {
+                // $user->bankAccounts()->save(factory(App\UserBankAccount::class)->create(['user_id' => $user->id]));
+                $user->transactions()->save(factory(App\Transaction::class)->create(['user_id' => $user->id]));
+            });
     }
 }
