f77a1700d3ebe941e1a382f58d8a4ccd77824cdc
DarkPain <darkpain87@gmail.com>
2017-06-11 12:34:29 +0300

    fix menu

--- config/services.php
+++ config/services.php
@@ -48,33 +48,33 @@ return [
         'facebook_token' => env('FACEBOOK_TOKEN'),
         'facebook_app_secret' => env('FACEBOOK_SECRET'),
         'facebook_start_button_payload' => 'START_CONVERSATION',
-        'facebook_persistent_menu'      => [
-            'persistent_menu' => [
-                [
-                    'locale'                  => 'default',
-                    'composer_input_disabled' => 'true',
-                    'call_to_actions'         => [
-                        [
-                            'title'           => 'My Account',
-                            'type'            => 'nested',
-                            'call_to_actions' => [
-                                [
-                                    'title'   => 'Pay Bill',
-                                    'type'    => 'postback',
-                                    'payload' => 'PAYBILL_PAYLOAD',
-                                ],
-                            ],
-                        ],
-                        [
-                            'type'                 => 'web_url',
-                            'title'                => 'Latest News',
-                            'url'                  => env('APP_URL'),
-                            'webview_height_ratio' => 'full',
-                        ],
-                    ],
-                ],
-            ],
-        ],
+//        'facebook_persistent_menu'      => [
+//            'persistent_menu' => [
+//                [
+//                    'locale'                  => 'default',
+//                    'composer_input_disabled' => 'true',
+//                    'call_to_actions'         => [
+//                        [
+//                            'title'           => 'My Account',
+//                            'type'            => 'nested',
+//                            'call_to_actions' => [
+//                                [
+//                                    'title'   => 'Pay Bill',
+//                                    'type'    => 'postback',
+//                                    'payload' => 'PAYBILL_PAYLOAD',
+//                                ],
+//                            ],
+//                        ],
+//                        [
+//                            'type'                 => 'web_url',
+//                            'title'                => 'Latest News',
+//                            'url'                  => env('APP_URL'),
+//                            'webview_height_ratio' => 'full',
+//                        ],
+//                    ],
+//                ],
+//            ],
+//        ],
     ],
 
 ];
