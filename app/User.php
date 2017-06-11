f58a39c88274af4d8593b6f23fa6aa49a5dc01fd
DarkPain <darkpain87@gmail.com>
2017-06-11 14:59:20 +0300

    user fix creation

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -48,7 +49,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    
+
     protected $dates = ['deleted_at'];
 
     protected $table = 'users';
